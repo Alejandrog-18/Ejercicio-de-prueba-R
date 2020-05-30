0f1621c6bcc4435eb2dfacb50f0da59aa1797a7e
Alejandrog <alejandrogarrido95@gmail.com>
2020-05-30 05:45:45 -0400

    Agregar prueba para guardar ejercicio en R

---
+++ prueba guardar R.R
@@ -0,0 +1,8 @@
+#Ejercicio de prueba: lanzar un dado que cuando salga un numero par usted
+#obtiene el doble de su puntaje mas 5, si es impar usted obtiene el triple
+#de su puntaje. ademas si en el primer lanzamiento usted saca un 1 o 6,
+#obtiene el doble de puntaje y debe volver a lanzar y seguir las reglas 
+#relacionadas con el puntaje par o impar.
+
+
+  
